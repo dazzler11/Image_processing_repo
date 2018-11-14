49b88ee392d602e21debeb3a6b24810a78c3fb51
dazzler17 <jitu.abhijit22@gmail.com>
2018-11-14 19:14:09 +0530

    modified

--- Deploy from RStudio to Azure.R
+++ Deploy from RStudio to Azure.R
@@ -4,6 +4,7 @@
 # install.packages("randomForest"): https://www.stat.berkeley.edu/~breiman/RandomForests/cc_home.htm
 # install.packages("AzureML"): https://github.com/RevolutionAnalytics/AzureML
 # install RTools : https://cran.r-project.org/bin/windows/Rtools/
+install_cran("https://cran.r-project.org/bin/windows/Rtools/")
 
 # Get IRIS dataset:
 library(RCurl)
@@ -12,6 +13,8 @@ library(RCurl)
 iris_url = "http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 iris_txt = getURL(iris_url)
 iris_data = read.csv(textConnection(iris_txt), header = FALSE)
+View(iris)
+View(iris_data)
 
 #Rename the columns:
 library(plyr)
@@ -19,6 +22,7 @@ names(iris_data)
 iris = rename(iris_data, c("V1"="Sepal_Length", "V2"="Sepal_Width","V3"="Petal_Length", "V4"="Petal_Width", "V5"="Species"))
 names(iris)
 irisInputs = iris[,-5]
+View(irisInputs)
 
 # Use Random Forest: 
 library(randomForest)
@@ -38,7 +42,7 @@ mypredict <- function(newdata)
 }
 
 
-print(mypredict(iris))
+ 
 
 
 if(!require("devtools")) install.packages("devtools")
@@ -48,17 +52,17 @@ devtools::install_github("RevolutionAnalytics/azureml")
 library(AzureML)
 library(devtools)
 
-wsID = "<Workspace ID>"
-wsAuth = "<Workspace Authorization Token>"
+wsID = "83db3a02a93f4322b9145667b9b30c18"
+wsAuth = "9LDsmjFujxbyxzT19Mh58e364j8Rd9M2//eDSJsyqhTK+7x9Ain1an1iDXPvEONzDlCrESeTeGJ4ParRld/E6w=="
 
 wsobj = workspace(wsID,wsAuth)
 
 # Create REST API:
-
+library(zipR)
 irisWebService <- publishWebService(
-  wsobj,
-  fun = mypredict, 
-  name = "irisWebService", 
-  inputSchema = irisInputs
+                                    wsobj,
+                                    fun = mypredict, 
+                                    name = "irisWebService", 
+                                    inputSchema = irisInputs
 )
 head(irisWebService)
